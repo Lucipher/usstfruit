e7ffe8ed2730779dcb878691dd3fdc74f0750621
Akira Panda <p.chenliang@gmail.com>
2014-09-05 11:24:28 +0800

    try

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -121,7 +121,7 @@ WeixinRailsMiddleware::WeixinController.class_eval do
              @articles.each do |article|
                arts = []
                # 商城首页
-               cover_url = article.cover.nil? ? "" : server_path + article.cover_url(:normal)
+               cover_url = article.cover.nil? ? "" : "#{server_path}#{article.cover_url(:normal)}"
                
                art_title = generate_article(article.title, article.breif,cover_url ,mobile_article_url(article))
                arts << art_title
