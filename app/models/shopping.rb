bc89b9ce83e0b36442a92edae4fc9de7ee127ad8
Akira Panda <p.chenliang@gmail.com>
2014-09-16 23:29:56 +0800

    add basic validtion

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,6 +1,7 @@
 class Shopping < ActiveRecord::Base
   has_many :shopping_items
   after_create :save_shopping_items
+  validates :customer_name,:customer_address,:mobile,:presence => true
   
   
   def save_shopping_items
