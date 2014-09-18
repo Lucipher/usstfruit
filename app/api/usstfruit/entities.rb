1091522e25ec9ade26da40a12931cc8d9d6573ad
Akira Panda <p.chenliang@gmail.com>
2014-09-18 16:29:11 +0800

    Add amount to api list

--- app/api/usstfruit/entities.rb
+++ app/api/usstfruit/entities.rb
@@ -4,11 +4,10 @@ module Usstfruit
     class ShoppingItem  < Grape::Entity
       expose :id, :no, :name, :price, :quantity, :amount
     end
-    
+  
     class Shopping   < Grape::Entity
-      expose :id, :customer_name, :customer_address, :mobile, :created_at, :status
+      expose :id, :customer_name, :customer_address, :mobile, :created_at, :status,:amount
       expose :shopping_items, :using => Entities::ShoppingItem, :unless => { :collection => true }
-      
     end
     
 
