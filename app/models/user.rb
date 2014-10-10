775baa482cb308a0f3063083211cde1dcae07319
Akira Panda <p.chenliang@gmail.com>
2014-10-10 10:56:50 +0800

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def admin?
-     self.email == "akirapanda@newnil.com" || self.role == "管理员" || self.email == "panxingyu@nosweetnopay.com" 
-     || self.email == "chentong0914@hotmail.com"
+     self.email == "akirapanda@newnil.com" || self.role == "管理员" || self.email == "panxingyu@nosweetnopay.com" || self.email == "chentong0914@hotmail.com"
   end
 end
